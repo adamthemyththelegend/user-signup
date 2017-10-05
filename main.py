1b70df1f06688e64c322bf227792ee6526da7207
Adam <agm500300@gmail.com>
2017-10-05 14:36:51 -0500

    tried to fix stuff but it didn't work

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import cgi
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-username = ""
-email = ""
-
 
 @app.route('/welcome', methods=['POST'])
 def welcome():
@@ -14,17 +11,17 @@ def welcome():
     password = request.form['password']
     verifypassword = request.form['passwordverify']
     if len(username) < 3 or len(username) > 20:
-        nameerror = "Please choose a username between 3 and 20 characters."
+        nameerror = "<p class='error'>Please choose a username between 3 and 20 characters.</p>"
         return render_template('login.html') + nameerror
     for character in username:
         if character == " ":
-            nameerror = "Your username cannot contain any spaces."
+            nameerror = "<p class='error'>Your username cannot contain any spaces.</p>"
             return render_template('login.html') + nameerror
     if (not password) or (password.strip() == ""):
-        nopwerror = "Please enter a password."
+        nopwerror = "<p class='error'>Please enter a password.</p>"
         return render_template('login.html') + nopwerror
     if password != verifypassword:
-        pwerror = "Your passwords do not match."
+        pwerror = "<p class='error'>Your passwords do not match.</p>"
         return render_template('login.html') + pwerror
     return render_template('welcome.html', username=username)
 
@@ -35,7 +32,7 @@ def index():
     usererror = request.args.get("nameerror")
     nopasserror = request.args.get("nopwerror")
     passerror = request.args.get("pwerror")
-    return render_template('login.html', 
+    return render_template('login.html',
     usererror=usererror and cgi.escape(usererror, quote=True), 
     nopasserror=nopasserror and cgi.escape(nopasserror, quote=True), 
     passerror=passerror and cgi.escape(passerror, quote=True))
